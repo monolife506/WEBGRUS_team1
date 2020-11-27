5f5a0a16e2a191fa9ba2bd4d4612ad274f18d84d
sohyeonjin <shj41738866@gmail.com>
2020-11-27 13:19:57 +0900

    modified navbar with menubar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,12 @@
 import React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Navbar from "./component/bar/Navbar";
-import Footer from "./component/Footer";
+import Footer from "./component/bar/Footer";
 import Main from "./component/Main";
 import LoginPage from "./component/LoginPage";
 import RegisterPage from "./component/RegisterPage";
 import Mypage from "./component/Mypage";
-import Newpost from "./component/fileUpload/Newpost";
+import Newpost from "./component/Newpost";
 import Auth from './hoc/auth'
 
 function App() {
