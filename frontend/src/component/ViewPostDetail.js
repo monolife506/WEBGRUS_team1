3526ea1dc9a280825a63fcd84675fa2c9a7bcce5
sohyeonjin <shj41738866@gmail.com>
2021-01-09 16:57:28 +0900

    update post detail with react-slick

--- frontend/src/component/ViewPostDetail.js
+++ frontend/src/component/ViewPostDetail.js
@@ -1,7 +1,10 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, Component } from "react";
 import { useParams } from "react-router-dom";
-import axios from "axios";
 import { SERVER_API } from "../_actions/config";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import "../component/_slick-theme.css";
 
 import { useDispatch } from "react-redux";
 import { getPostDetail } from "../_actions/postAction";
@@ -22,30 +25,46 @@ function ViewPostDetail() {
     });
   }, []);
 
+  const slickSettings = {
+    dots: true,
+    infinite: false,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: true,
+  };
+
   return (
     <div style={{ display: "flex", justifyContent: "center" }}>
       <div
         style={{
           borderStyle: "solid",
           width: 900,
-          height: 1200,
           margin: "5px 5px 5px 10px",
         }}
       >
-        <div
-          style={{
-            display: "flex",
-            justifyContent: "space-around",
-            flexWrap: "wrap",
-          }}
-        >
+        <div style={{ display: "flex", justifyContent: "center" }}>
           {/* 사진들 보여주기 */}
-          {Photos.map((photo) => (
-            <img
-              src={`${SERVER_API}/${photo.path}`}
-              style={{ width: 290, height: 290 }}
-            />
-          ))}
+          <Slider
+            {...slickSettings}
+            style={{
+              borderStyle: "solid",
+              width: 500,
+            }}
+          >
+            {Photos.map((photo) => (
+              <div>
+                <img
+                  src={`${SERVER_API}/${photo.path}`}
+                  // src='https://postfiles.pstatic.net/MjAyMDEyMDRfNDgg/MDAxNjA3MDkzOTY5NzQx.aiWaOE9spN6qRdX5Yxc3TVnGmkbTYosj9X0j9o3Fh8Ig.Z5qNMeLnm5CgOvLO1iNfqm9x3H4FrQbu527d82qXxbUg.JPEG.wlduddl9604/IMG_5736.jpg?type=w580'
+                  style={{
+                    width: 500,
+                    height: 500,
+                  }}
+                />
+              </div>
+            ))}
+          </Slider>
         </div>
 
         <div
