3da4af02cea4585ecc1d15a346e39e6f31af0721
sohyeonjin <shj41738866@gmail.com>
2021-01-25 19:38:11 +0900

    Add loading component for mypage

---
+++ frontend/src/component/Loading.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+function Loading() {
+  return (
+    <div
+      style={{
+        height: "90vh",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      Loading...
+    </div>
+  );
+}
+
+export default Loading;
