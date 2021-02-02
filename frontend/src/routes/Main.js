ca5ee382fd9d6f9f6b5b15ce8ecc35c9d892d0ba
An Yong Mo <aym506@outlook.com>
2021-02-02 22:21:03 +0900

    Implement detecting end scroll

--- frontend/src/routes/Main.js
+++ frontend/src/routes/Main.js
@@ -1,31 +1,33 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { connect, useDispatch } from "react-redux";
 
 import { getAllpost } from "../_actions/postAction";
 
 import Post from "../component/Post";
 import Loading from "../component/Loading";
-import Axios from "axios";
-import { SERVER_API } from "../_actions/config";
+import useInfinteScroll from "../hoc/infiniteScroll";
 
 function Main(props) {
   const dispatch = useDispatch();
-
   const [Posts, setPosts] = useState([]);
+  const [sortMode, setSortMode] = useState('times');
 
-  //모든 포스트 가져오기
+  const targetRef = useRef(null);
+
   useEffect(() => {
-    dispatch(getAllpost()).then((res) => {
+    dispatch(getAllpost.load('times', 0)).then((res) => {
       setPosts(res.payload);
     });
-  }, []);
-  const PostSorting = (e, sort) => {
-    console.log(sort);
-    Axios.get(`${SERVER_API}/api/posts/all?sortby=${sort}`).then((res) => {
-      console.log(res);
-      setPosts(res.data);
-    });
-  };
+  }, [sortMode, Posts]);
+
+
+  useInfinteScroll({
+    target: targetRef.current,
+    onIntersect: ([{ isIntersecting }]) => {
+      if (isIntersecting) console.log("end!")
+    }
+  });
+
 
   //모든 포스트 가져오는 액션이 끝난 후 로드
   if (props.post.allpost) {
@@ -36,8 +38,8 @@ function Main(props) {
           <button
             type='button'
             name='times'
-            onClick={(e) => {
-              PostSorting(e, "times");
+            onClick={() => {
+              setSortMode('times');
             }}
           >
             최신순
@@ -45,8 +47,8 @@ function Main(props) {
           <button
             type='button'
             name='views'
-            onClick={(e) => {
-              PostSorting(e, "views");
+            onClick={() => {
+              setSortMode('views');
             }}
           >
             조회순
@@ -54,14 +56,13 @@ function Main(props) {
           <button
             type='button'
             name='likes'
-            onClick={(e) => {
-              PostSorting(e, "likes");
+            onClick={() => {
+              setSortMode('likes');
             }}
           >
             인기순
           </button>
         </div>
-
         <div style={{ display: "flex", justifyContent: "center" }}>
           <div
             style={{
@@ -73,33 +74,34 @@ function Main(props) {
           >
             {Posts
               ? Posts.map((post) => (
-                  <div
+                <div
+                  key={post._id}
+                  style={{
+                    display: "flex",
+                    justifyContent: "space-between",
+                    flexWrap: "wrap",
+                  }}
+                >
+                  <Post
                     key={post._id}
-                    style={{
-                      display: "flex",
-                      justifyContent: "space-between",
-                      flexWrap: "wrap",
-                    }}
-                  >
-                    <Post
-                      key={post._id}
-                      postid={post._id}
-                      owner={post.owner}
-                      title={post.title}
-                      description={post.description}
-                      files={post.files}
-                      tags={post.tags}
-                      posttime={post.posttime}
-                      likecnt={post.likecnt}
-                      viewcnt={post.viewcnt}
-                      commentcnt={post.commentcnt}
-                      auth={props.auth.userData}
-                    />
-                  </div>
-                ))
+                    postid={post._id}
+                    owner={post.owner}
+                    title={post.title}
+                    description={post.description}
+                    files={post.files}
+                    tags={post.tags}
+                    posttime={post.posttime}
+                    likecnt={post.likecnt}
+                    viewcnt={post.viewcnt}
+                    commentcnt={post.commentcnt}
+                    auth={props.auth.userData}
+                  />
+                </div>
+              ))
               : ""}
           </div>
         </div>
+        <div ref={targetRef} />
       </>
     );
   } else {
