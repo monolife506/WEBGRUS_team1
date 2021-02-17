f976dcf453a33e7496997d73cca0d957f71a9dc1
sohyeonjin <shj41738866@gmail.com>
2021-02-17 20:49:42 +0900

    Update css

--- frontend/src/component/bar/menubar.js
+++ frontend/src/component/bar/menubar.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import Menu from "@material-ui/core/Menu";
 import MenuItem from "@material-ui/core/MenuItem";
 import { Link, useHistory } from "react-router-dom";
-import { useDispatch } from "react-redux";
+import { connect, useDispatch } from "react-redux";
 import { logoutUser } from "../../_actions/authAction";
 import "./bar.scss";
 
@@ -25,45 +25,46 @@ function SimpleMenu(props) {
     dispatch(logoutUser());
     history.push("/login");
   };
+  if (props.auth.status !== "LOADING")
+    return (
+      <div className='nav'>
+        <button
+          aria-controls='menubar'
+          aria-haspopup='true'
+          onClick={handleClick}
+        >
+          {props.auth.userData}
+        </button>
+        <Menu
+          id='menubar'
+          anchorEl={anchorEl}
+          keepMounted
+          open={Boolean(anchorEl)}
+          onClose={handleClose}
+        >
 
-  return (
-    <div className='nav'>
-      <button
-        aria-controls='menubar'
-        aria-haspopup='true'
-        onClick={handleClick}
-      >
-        마이페이지
-      </button>
-      <Menu
-        id='menubar'
-        anchorEl={anchorEl}
-        keepMounted
-        open={Boolean(anchorEl)}
-        onClose={handleClose}
-      >
-        {/* <MenuItem>
-          <Link
-            to={{ pathname: "" }}
-            style={{ textDecoration: "none", color: "black" }}
-          >
-            내 계정
-          </Link>
-        </MenuItem> */}
+          <MenuItem>
+            <Link to={{ pathname: "/newpost" }}> 디자인올리기</Link>
+          </MenuItem>
 
-        <MenuItem>
-          <Link
-            to={{ pathname: "/mypage" }}
-            style={{ textDecoration: "none", color: "black" }}
-          >
-            마이페이지
-          </Link>
-        </MenuItem>
+          <MenuItem>
+            <Link
+              to={{ pathname: "/mypage" }}
+              style={{ textDecoration: "none", color: "black" }}
+            >
+              마이페이지
+            </Link>
+          </MenuItem>
 
-        <MenuItem onClick={onLogout}>로그아웃</MenuItem>
-      </Menu>
-    </div>
-  );
+          <MenuItem onClick={onLogout}>로그아웃</MenuItem>
+          
+        </Menu>
+      </div>
+    );
 }
 
-export default SimpleMenu;
+const mapStateToProps = (state) => ({
+  auth: state.auth,
+});
+
+export default connect(mapStateToProps)(SimpleMenu);
