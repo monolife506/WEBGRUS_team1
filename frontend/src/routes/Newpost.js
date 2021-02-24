97b0f06b6cc5f579b6288fca03df230badb6f577
sohyeonjin <shj41738866@gmail.com>
2021-02-24 13:54:58 +0900

    Update css

--- frontend/src/routes/Newpost.js
+++ frontend/src/routes/Newpost.js
@@ -1,15 +1,45 @@
 import React, { useState, useEffect } from "react";
-import Button from "@material-ui/core/Button";
-import TextField from "@material-ui/core/TextField";
 import Dropzone from "react-dropzone";
-import AddIcon from "@material-ui/icons/Add";
 import { withRouter } from "react-router-dom";
 
-// import Chip from "@material-ui/core/Chip";
-// import Autocomplete from "@material-ui/lab/Autocomplete";
+import AddIcon from "@material-ui/icons/Add";
+import CloseIcon from "@material-ui/icons/CloseOutlined";
+
+import styled from "styled-components";
 
 import { useDispatch, useSelector } from "react-redux";
 import { fileUpload } from "../_actions/postAction";
+
+const StyleDiv = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: left;
+  align-items: flex-start;
+  min-width: 380px;
+  width: 55vw;
+`;
+
+const StyleLabel = styled.div`
+  display: inline-block;
+  margin: 5px 0 10px 0;
+  width: 5vw;
+  min-width: 70px;
+  text-align: center;
+  font-family: "notoBold";
+`;
+
+const StyleInput = styled.input`
+  margin: 5px 2vw 10px 2vw;
+  width: 40vw;
+  min-width: 360px;
+`;
+
+const StyleButton = styled.button`
+  width: 50px;
+  height: 25px;
+  background-color: black;
+  color: white;
+`;
 
 function Newpost(props) {
   const auth = useSelector((state) => state.auth);
@@ -67,6 +97,46 @@ function Newpost(props) {
     }
   };
 
+  //드랍존
+  const dropZone = (
+    <Dropzone accept='image/*' onDrop={onDrop}>
+      {({ getRootProps, getInputProps }) => (
+        <section
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            margin: "2vh 2vw 2vh 2vw",
+          }}
+        >
+          <div
+            {...getRootProps()}
+            style={{
+              width: `calc(40vw + 4px)`,
+              height: "30vh",
+              minWidth: 360,
+              minHeight: 100,
+              backgroundColor: "lightgray",
+              display: "flex",
+              flexDirection: "column",
+              justifyContent: "center",
+              alignItems: "center",
+              border: "dashed 3px gray",
+            }}
+          >
+            <input {...getInputProps()} />
+            <AddIcon />
+            <p style={{ margin: "3vh 0 3vh 0" }}>
+              이미지 파일을 선택하거나 끌어다 놓으세요
+            </p>
+            <p style={{ fontSize: "12px", margin: 0 }}>
+              (파일은 최대 6개까지 업로드 할 수 있습니다.)
+            </p>
+          </div>
+        </section>
+      )}
+    </Dropzone>
+  );
+
   //썸네일 보여주기
   const ThumbnailView = thumbnails.map((thumb) => (
     <div
@@ -78,7 +148,6 @@ function Newpost(props) {
         width: "150px",
         height: "150px",
         border: "dashed 1px gray",
-        margin: "0 10px 0 10px",
       }}
     >
       <img
@@ -146,134 +215,120 @@ function Newpost(props) {
     }
   };
 
-  //input style
-  const Textfield = {
-    margin: "5px 0 10px 0",
-    width: "40vw",
-    minWidth: "400px",
-  };
-
   return (
     <div
-      style={{
-        display: "flex",
-        alignItems: "center",
-        flexDirection: "column",
-      }}
+      style={{ display: "flex", alignItems: "center", flexDirection: "column" }}
     >
-      <h1>UPLOAD</h1>
-      <TextField
-        style={Textfield}
-        required
-        id='standard-required'
-        label='제목'
-        variant='outlined'
-        onChange={onTitle}
-      />
-      <TextField
-        style={Textfield}
-        id='standard-multiline-static'
-        label='작품소개'
-        multiline
-        rows={4}
-        variant='outlined'
-        onChange={onDescription}
-      />
-      {/* 태그입력창 */}
-      <div>
-        <TextField
-          id='standard-multiline-static'
-          value={currentTag}
-          name='tag'
-          label='태그'
-          variant='outlined'
-          onChange={onCurrentTag}
-          onKeyPress={onTagKeyPress}
-        />
-        <Button
-          type='button'
-          onClick={onTagClick}
-          variant='contained'
-          color='primary'
-        >
-          추가
-        </Button>
-        {/* 태그를 추가하면 태그 나타내기 */}
-        {tags
-          ? tags.map((tag) => (
-              <div key={tag}>
-                #{tag}
-                <button
-                  type='button'
-                  onClick={(e) => {
-                    e.preventDefault();
-                    const deleteTag = tag;
-                    setTags(tags.filter((tag) => tag !== deleteTag));
-                  }}
-                >
-                  삭제
-                </button>
-              </div>
-            ))
-          : ""}
-      </div>
-      <Dropzone accept='image/*' onDrop={onDrop}>
-        {({ getRootProps, getInputProps }) => (
-          <section
-            style={{
-              display: "flex",
-              justifyContent: "center",
-              marginTop: "5vh",
-            }}
-          >
-            <div
-              {...getRootProps()}
-              style={{
-                width: "40vw",
-                height: "30vh",
-                minWidth: "400px",
-                minHeight: "100px",
-                backgroundColor: "lightgray",
-                display: "flex",
-                flexDirection: "column",
-                justifyContent: "center",
-                alignItems: "center",
-                border: "dashed 3px gray",
-              }}
-            >
-              <input {...getInputProps()} />
-              <AddIcon />
-              <p style={{ margin: "3vh 0 3vh 0" }}>
-                이미지 파일을 선택하거나 끌어다 놓으세요
-              </p>
-              <p style={{ fontSize: "12px", margin: 0 }}>
-                (파일은 최대 6개까지 업로드 할 수 있습니다.)
-              </p>
-            </div>
-          </section>
-        )}
-      </Dropzone>
+      <h1 style={{ fontFamily: "notoBold" }}>UPLOAD</h1>
       <div
         style={{
-          margin: "5vh 0 5vh 0",
+          position: "relative",
           display: "flex",
           justifyContent: "center",
-          alignItems: "center",
-          width: "1000px",
-          overflowX: "auto",
-          overflowY: "hidden",
+          width: "80vw",
+          padding: "4vh 0 50px 0",
+          backgroundColor: "#FFFFFF",
         }}
       >
-        {ThumbnailView}
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "left",
+            margin: "0 auto",
+          }}
+        >
+          <StyleDiv>
+            <StyleLabel style={{ height: 0 }}></StyleLabel>
+            <div
+              style={{
+                margin: "3vh 2vw",
+                minWidth: 360,
+                width: "40vw",
+                display: "flex",
+                justifyContent: "left",
+                alignItems: "center",
+                overflowX: "auto",
+                overflowY: "hidden",
+              }}
+            >
+              {ThumbnailView}
+            </div>
+          </StyleDiv>
+          <StyleDiv>
+            <StyleLabel style={{ height: 0 }}></StyleLabel>
+            {dropZone}
+          </StyleDiv>
+          <StyleDiv>
+            <StyleLabel>제목</StyleLabel> <StyleInput onChange={onTitle} />
+          </StyleDiv>
+          <StyleDiv>
+            <StyleLabel>작품소개</StyleLabel>
+            <StyleInput style={{ height: "30vh" }} onChange={onDescription} />
+          </StyleDiv>
+
+          {/* 태그입력창 */}
+          <StyleDiv>
+            <StyleLabel>태그</StyleLabel>
+            <div style={{ margin: "5px 2vw 10px 2vw" }}>
+              <StyleInput
+                style={{
+                  width: `calc(38vw - 50px)`,
+                  minWidth: `calc(310px - 2vw)`,
+                  margin: "0 2vw 0 0",
+                }}
+                value={currentTag}
+                name='tag'
+                label='태그'
+                onChange={onCurrentTag}
+                onKeyPress={onTagKeyPress}
+              />
+              <StyleButton type='button' onClick={onTagClick}>
+                추가
+              </StyleButton>
+            </div>
+          </StyleDiv>
+          <div
+            style={{ display: "flex", flexdirection: "wrap", marginLeft: 70 }}
+          >
+            {/* 태그를 추가하면 태그 나타내기 */}
+            {tags
+              ? tags.map((tag) => (
+                  <div key={tag}>
+                    #{tag}
+                    <button
+                      type='button'
+                      onClick={(e) => {
+                        e.preventDefault();
+                        const deleteTag = tag;
+                        setTags(tags.filter((tag) => tag !== deleteTag));
+                      }}
+                      style={{backgroundColor:'#FFFFFF', textAlign:32}}
+                    >
+                      <CloseIcon fontSize='small' color='action' />
+                    </button>
+                  </div>
+                ))
+              : ""}
+          </div>
+        </div>
+
+        <StyleButton
+          type='submit'
+          variant='contained'
+          color='primary'
+          onClick={BlockDoubleSubmit}
+          style={{
+            width: 70,
+            position: "absolute",
+            display: "block",
+            right: 30,
+            bottom: 20,
+          }}
+        >
+          올리기
+        </StyleButton>
       </div>
-      <Button
-        type='submit'
-        variant='contained'
-        color='primary'
-        onClick={BlockDoubleSubmit}
-      >
-        올리기
-      </Button>
     </div>
   );
 }
