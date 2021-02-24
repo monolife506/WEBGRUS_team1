c53b8c1e8b7a731bee2c088d61b39395afc8be87
sohyeonjin <shj41738866@gmail.com>
2021-02-24 13:50:13 +0900

    Remove meterial-ui container

--- frontend/src/routes/LoginPage.js
+++ frontend/src/routes/LoginPage.js
@@ -1,42 +1,15 @@
 import React, { useState } from "react";
 import Button from "@material-ui/core/Button";
-import CssBaseline from "@material-ui/core/CssBaseline";
-import TextField from "@material-ui/core/TextField";
 import Link from "@material-ui/core/Link";
 import Grid from "@material-ui/core/Grid";
-import Typography from "@material-ui/core/Typography";
-import { makeStyles } from "@material-ui/core/styles";
-import Container from "@material-ui/core/Container";
 import { loginUser } from "../_actions/authAction";
 
 import { useDispatch } from "react-redux";
 import { useHistory } from "react-router-dom";
 
-
-const useStyles = makeStyles((theme) => ({
-  paper: {
-    marginTop: theme.spacing(2),
-    display: "flex",
-    flexDirection: "column",
-    alignItems: "center",
-  },
-  avatar: {
-    margin: theme.spacing(1),
-    backgroundColor: theme.palette.secondary.main,
-  },
-  form: {
-    width: "100%", // Fix IE 11 issue.
-    marginTop: theme.spacing(1),
-  },
-  submit: {
-    margin: theme.spacing(3, 0, 2),
-  },
-}));
-
 function SignIn() {
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
   const history = useHistory();
-  const classes = useStyles();
 
   const [id, setId] = useState("");
   const [password, setPassword] = useState("");
@@ -47,6 +20,11 @@ function SignIn() {
   const onPassword = (e) => {
     setPassword(e.currentTarget.value);
   };
+
+  const onSubmitpress = (e) => {
+    if (e.key === "Enter") onSubmit(e);
+  };
+
   const onSubmit = async (e) => {
     e.preventDefault();
 
@@ -69,63 +47,36 @@ function SignIn() {
   };
 
   return (
-    <Container component='main' maxWidth='xs'>
-      <CssBaseline />
-      <div className={classes.paper}>
-        <Typography component='h1' variant='h5'>
-          로그인
-        </Typography>
-        <form className={classes.form} onSubmit={onSubmit}>
-          <TextField
-            variant='outlined'
-            margin='normal'
-            required
-            fullWidth
-            id='id'
-            label='아이디'
+    <>
+      <div>
+        <h3>로그인</h3>
+        <form onSubmit={onSubmit}>
+          <input
+            type='text'
             value={id}
-            autoComplete='id'
+            placeholder='아이디'
             autoFocus
             onChange={onId}
           />
-          <TextField
-            variant='outlined'
-            margin='normal'
-            required
-            fullWidth
+          <input
+            type='password'
             value={password}
             label='비밀번호'
-            type='password'
-            id='password'
-            autoComplete='current-password'
+            placeholder='비밀번호'
             onChange={onPassword}
+            onKeyPress={onSubmitpress}
           />
-          <Button
-            type='submit'
-            fullWidth
-            variant='contained'
-            color='primary'
-            className={classes.submit}
-          >
-            로그인
-          </Button>
+          <button type='submit'>로그인</button>
           <Grid container>
-            <Grid item xs>
-              <Link href='#' variant='body2'>
-                비밀번호를 잊어버렸나요?
-              </Link>
-            </Grid>
+            <Grid item xs></Grid>
             <Grid item>
-              <Link href='/register' variant='body2'>
-                {"계정이 없나요? 회원가입"}
-              </Link>
+              <Link href='/register'>{"계정이 없나요? 회원가입"}</Link>
             </Grid>
           </Grid>
         </form>
       </div>
-    </Container>
+    </>
   );
 }
-
 
 export default SignIn;
--- frontend/src/routes/RegisterPage.js
+++ frontend/src/routes/RegisterPage.js
@@ -1,38 +1,15 @@
 import React, { useState } from "react";
-import Button from "@material-ui/core/Button";
-import CssBaseline from "@material-ui/core/CssBaseline";
-import TextField from "@material-ui/core/TextField";
 import Link from "@material-ui/core/Link";
 import Grid from "@material-ui/core/Grid";
-import Typography from "@material-ui/core/Typography";
-import { makeStyles } from "@material-ui/core/styles";
-import Container from "@material-ui/core/Container";
+
 import { registerUser } from "../_actions/authAction";
 
 import { useHistory } from "react-router-dom";
 import { useDispatch } from "react-redux";
 
-const useStyles = makeStyles((theme) => ({
-  paper: {
-    marginTop: theme.spacing(2),
-    display: "flex",
-    flexDirection: "column",
-    alignItems: "center",
-  },
-  form: {
-    width: "100%", // Fix IE 11 issue.
-    marginTop: theme.spacing(3),
-  },
-  submit: {
-    margin: theme.spacing(3, 0, 2),
-  },
-}));
-
 function SignUp(props) {
   const history = useHistory();
   const dispatch = useDispatch();
-
-  const classes = useStyles();
 
   const [id, setId] = useState("");
   const [email, setEmail] = useState("");
@@ -53,6 +30,10 @@ function SignUp(props) {
     await setPasswordcheck(e.target.value);
     if (e.target.value !== password) setPasswordchecking(false);
     else setPasswordchecking(true);
+  };
+
+  const onSubmitpress = (e) => {
+    if (e.key === "Enter") onSubmit(e);
   };
 
   const onSubmit = async (e) => {
@@ -80,79 +61,50 @@ function SignUp(props) {
   };
 
   return (
-    <Container component='main' maxWidth='xs'>
-      <CssBaseline />
-      <div className={classes.paper}>
-        <Typography component='h1' variant='h5'>
-          회원가입
-        </Typography>
-        <form className={classes.form} onSubmit={onSubmit}>
+    <>
+      <div>
+        <h3>회원가입</h3>
+        <form onSubmit={onSubmit}>
           <Grid container spacing={2}>
             <Grid item xs={12}>
-              <TextField
-                variant='outlined'
-                required
-                fullWidth
-                id='id'
+              <input
+                type='text'
                 value={id}
-                label='아이디'
-                autoComplete='id'
+                placeholder='아이디'
                 onChange={onId}
               />
             </Grid>
             <Grid item xs={12}>
-              <TextField
-                variant='outlined'
-                required
-                fullWidth
-                id='email'
+              <input
                 type='email'
                 value={email}
-                label='이메일'
-                autoComplete='email'
+                placeholder='이메일'
                 onChange={onEmail}
               />
             </Grid>
             <Grid item xs={12}>
-              <TextField
-                variant='outlined'
-                required
-                fullWidth
-                value={password}
+              <input
                 type='password'
-                id='password'
-                label='비밀번호'
-                autoComplete='current-password'
+                value={password}
+                placeholder='비밀번호'
                 onChange={onPassword}
               />
             </Grid>
             <Grid item xs={12}>
-              <TextField
+              <input
                 error={!passwordchecking}
                 helperText={
                   !passwordchecking ? "비밀번호와 일치하지 않습니다." : ""
                 }
-                variant='outlined'
-                required
-                fullWidth
-                value={passwordcheck}
                 type='password'
-                id='passwordcheck'
-                label='비밀번호 확인'
-                autoComplete='current-password'
+                value={passwordcheck}
+                placeholder='비밀번호 확인'
                 onChange={onPasswordcheck}
+                onKeyPress={onSubmitpress}
               />
             </Grid>
           </Grid>
-          <Button
-            type='submit'
-            fullWidth
-            variant='contained'
-            color='primary'
-            className={classes.submit}
-          >
-            회원가입
-          </Button>
+          <button type='submit'>확인</button>
           <Grid container justify='flex-end'>
             <Grid item>
               <Link href='/login' variant='body2'>
@@ -162,7 +114,7 @@ function SignUp(props) {
           </Grid>
         </form>
       </div>
-    </Container>
+    </>
   );
 }
 
